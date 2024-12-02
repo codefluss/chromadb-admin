000b8a4a43c6076d2b0cd56b37031df1c1d40cc1
Friedrich Bachinger <codefluss@gmail.com>
2024-12-02 23:53:35 +0100

    minor bug fixes in ci cd

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.module.rules.push({
       test: /\.node$/,
